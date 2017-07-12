3e9a2163050d9ddea792b1939ce58a041cb58e86
Willian Rocha <will.vdr@gmail.com>
2017-07-12 20:27:29 -0300

    refactor

---
+++ tsedata/__init__.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+
+from .tsedata import GetTSEData, TSEDump
+
+
+class TSE:
+    def __init__(self):
+        _party_list = ['DEM', 'NOVO', 'PCB', 'PCO', 'PC_do_B', 'PDT', 'PEN',
+        'PHS', 'PMB', 'PMDB', 'PMN', 'PP', 'PPL', 'PPS', 'PR',
+        'PRB', 'PROS', 'PRP', 'PRTB', 'PSB', 'PSC', 'PSD', 'PSDB',
+        'PSDC', 'PSL', 'PSOL', 'PSTU', 'PT', 'PTB', 'PTC', 'PTN',
+        'PT_do_B', 'PV', 'REDE', 'SD']
+        _uf_list = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO',
+        'MA', 'MT', 'MS', 'MG', 'PR', 'PB', 'PA', 'PE', 'PI', 'RJ',
+        'RN', 'RS', 'RO', 'RR', 'SC', 'SE', 'SP', 'TO']
+        self._party_list = _party_list
+        self._uf_list = _uf_list
+        self._csv_fields = csv_fields
+
+    def download(self, partido=None, estado=None):
+        if partido is None:
+            partido = self._party_list
+        if estado is None:
+            estado = self._uf_list
+        list_files = GetTSEData().getdata(party_list=partido,
+                                          uf_list=estado)
+        return list_files
+
+    def extract(self, partido=None, estado=None):
+        if partido is None:
+            partido = self._party_list
+        if estado is None:
+            estado = self._uf_list
+        filiados = TSEDump().filiation_data(party_list=partido,
+                                            uf_list=estado)
+        return filiados
--- tsedata/tsedata.py
+++ tsedata/tsedata.py
@@ -1,81 +1,130 @@
 import requests
 import hashlib
 from os import makedirs, path, remove
-from shutil import move
+from zipfile import ZipFile
+import json
+import csv
+from shutil import move, rmtree
 
-uf_list = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG',
-    'PR','PB','PA','PE','PI','RJ','RN','RS','RO','RR','SC','SE','SP','TO']
-party_list = ['DEM', 'NOVO', 'PCB', 'PCO', 'PC_do_B', 'PDT', 'PEN', 'PHS',
-    'PMB', 'PMDB', 'PMN', 'PP', 'PPL', 'PPS', 'PR', 'PRB', 'PROS', 'PRP',
-    'PRTB', 'PSB', 'PSC', 'PSD', 'PSDB', 'PSDC', 'PSL', 'PSOL', 'PSTU', 'PT',
-    'PTB', 'PTC', 'PTN', 'PT_do_B', 'PV', 'REDE', 'SD']
 
-'''
-Download all the 975 files of every party of every state, including DF
-'''
-def getdata( \
-    url='http://agencia.tse.jus.br/estatistica/sead/eleitorado/filiados/uf/'):
-    fname = 'filiados_{0}_{1}.zip'
-    url_complete = url + fname
-    rdirectory = 'files/'
-    tdirectory = 'files/tmp/'
-    for party in party_list:
-        for uf in uf_list:
-            file_name = fname.format(party,uf).lower()
-            print(url_complete.format(party,uf).lower())
-            download_file(url_complete.format(party,uf).lower(), file_name)
-            if not check_file(file_name,rdirectory,tdirectory):
-                move_file(file_name,tdirectory,rdirectory)
-            else:
-                remove(tdirectory+file_name)
+class GetTSEData(object):
+    _root_dir = 'files/'
+    _tmp_dir = 'tmp/'
 
-'''
-Download a single file base on the base url and the file name
-'''
-def download_file(url, fname):
-    directory = 'files/tmp/'
-    if not path.exists(directory):
-        makedirs(directory)
-    filename = directory + fname
-    r = requests.get(url, stream=True)
-    with open(filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-    return filename
+    '''
+    Download all the 975 files of every party of every state, including DF
+    '''
+    def getdata(self, party_list, uf_list,
+                rdirectory=_root_dir, tdirectory=_tmp_dir):
+        base = 'http://agencia.tse.jus.br/'
+        path = 'estatistica/sead/eleitorado/filiados/uf/'
+        fname = 'filiados_{0}_{1}.zip'
+        new_files = []
+        url_complete = base + path + fname
+        for party in party_list:
+            for uf in uf_list:
+                file_name = fname.format(party, uf).lower()
+                fn = self.download_file(url_complete.format(party, uf).lower(),
+                                        file_name, tdirectory)
+                if self.check_file(file_name, rdirectory, tdirectory):
+                    self.move_file(file_name, tdirectory, rdirectory)
+                    new_files.append(fn)
+                else:
+                    remove(tdirectory+file_name)
+        return new_files
 
-'''
-Create a md5 hash
-Stack Overflow questions/3431825
-'''
-def md5(fname):
-    hash_md5 = hashlib.md5()
-    with open(fname, 'rb') as f:
-        for chunk in iter(lambda: f.read(4096), b''):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+    '''
+    Download a single file base on the base url and the file name
+    '''
+    def download_file(self, url, fname, tdirectory):
+        directory = tdirectory
+        if not path.exists(directory):
+            makedirs(directory)
+        filename = directory + fname
+        r = requests.get(url, stream=True)
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+        return filename
 
-'''
-Check if the file was already downloaded
-'''
-def check_file(fname,ffrom,fto):
-    if path.isfile(ffrom+fname):
-        rfile = md5(ffrom+fname)
-    else:
-        return False
-    if path.isfile(fto+fname):
-        tfile = md5(fto+fname)
-    else:
-        print('check_file: No file downloaded')
-    if rfile == tfile:
-        return False
-    else:
-        return True
+    '''
+    Create a md5 hash
+    Stack Overflow questions/3431825
+    '''
+    def md5(self, fname):
+        hash_md5 = hashlib.md5()
+        with open(fname, 'rb') as f:
+            for chunk in iter(lambda: f.read(4096), b''):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
 
-'''
-Move files from one place to another
-'''
-def move_file(fname, ffrom, fto):
-    move(ffrom+fname,fto+fname)
+    '''
+    Check if the file was already downloaded
+    '''
+    def check_file(self, fname, ffrom, fto):
+        if path.isfile(ffrom+fname):
+            rfile = self.md5(ffrom+fname)
+        else:
+            return True
+        if path.isfile(fto+fname):
+            tfile = self.md5(fto+fname)
+        else:
+            raise FileNotFoundError('No file downloaded: {}'.format(fto+fname))
+        if rfile == tfile:
+            return False
+        else:
+            return True
 
-getdata()
+    '''
+    Move files from one place to another
+    '''
+    def move_file(self, fname, ffrom, fto):
+        move(ffrom+fname, fto+fname)
+
+
+class TSEDump(object):
+    _root_dir = 'files/'
+    _tmp_dir = 'tmp/'
+
+    def filiation_data(self, party_list, uf_list,
+                       rdirectory=_root_dir, tdirectory=_tmp_dir):
+        fname = 'filiados_{0}_{1}'
+        filiados = {}
+        for party in party_list:
+            for uf in uf_list:
+                name = fname.format(party, uf).lower()
+                fil = self.open_file(name, uf, party, rdirectory, tdirectory)
+                filiados[name] = fil
+        return filiados
+
+    def extract_zip(self, fname, tdirectory):
+        with ZipFile(fname, 'r') as zip_ref:
+            zip_ref.extractall(tdirectory)
+
+    def open_file(self, fname, uf, party, rdirectory, tdirectory):
+        csv_fields = ('data_da_extracao', 'hora_da_extracao', 'numero_da_inscricao',
+        'nome_do_filiado', 'sigla_do_partido', 'nome_do_partido', 'uf',
+        'codigo_do_municipio', 'nome_do_municipio', 'zona_eleitoral',
+        'secao_eleitoral', 'data_da_filiacao', 'situacao_do_registro',
+        'tipo_do_registro', 'data_do_processamento', 'data_da_desfiliacao',
+        'data_do_cancelamento', 'data_da_regularizacao', 'motivo_do_cancelamento')
+        csv_directory = '{}/aplic/sead/lista_filiados/uf/'.format(tdirectory)
+        csv_file = csv_directory + fname + '.csv'
+        zip_name = rdirectory + fname + '.zip'
+        collection_name = 'filiation_{}_{}'.format(party, uf).lower()
+        if path.isfile(zip_name):  # Check if the original file existis
+            self.extract_zip(zip_name, tdirectory)
+        else:
+            raise FileNotFoundError('No Zip Found: {}'.format(zip_name))
+        if path.isfile(csv_file):
+            csvfile = open(csv_file, 'r', encoding='iso-8859-1')
+            csv_entries = csv.DictReader(csvfile, csv_fields, delimiter=';')
+            json_list = []
+            for row in csv_entries:
+                acpt = json.dumps(row)
+                json_list.append(json.loads(acpt))
+        else:
+            raise FileNotFoundError('No CSV Found: {}'.format(csvfile))
+        rmtree(tdirectory)
+        return json_list
